d7ff1a7035c1dc1556927a868934c41a7e96da2e
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2023-11-30 10:54:47 -0500

    Relaciones modelos

--- src/main/java/co/com/parking/models/ServiceDetail.java
+++ src/main/java/co/com/parking/models/ServiceDetail.java
@@ -26,6 +26,15 @@ public class ServiceDetail extends GeneralModel{
     @JoinColumn(name="car_id")
     private Car car;
 
+    @ManyToOne
+    @JoinColumn(name="card_id")
+    private Card card;
+
+    @ManyToOne
+    @JoinColumn(name="service_id")
+    private Service service;
+
+
     public ServiceDetail(LocalDateTime entry_hour, LocalDateTime exit_hour) {
         this.entry_hour = entry_hour;
         this.exit_hour = exit_hour;
